1949091581cd498bd9773cbbbcd8975e0366449a
kairavrinda <42286909+kairavrinda@users.noreply.github.com>
2018-08-10 20:59:38 -0600

    Update bot_main.py

--- src/bot_main.py
+++ src/bot_main.py
@@ -6,6 +6,6 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 from pluralkit import bot
 
-pk = bot.PluralKitBot(os.environ["TOKEN"])
+pk = bot.PluralKitBot(os.environ["NDc3NjcxMjg2NzQ3OTU1MjIx.Dk_iJA.eEM2OZ6elVEAx4u3Ewe2njp-1r8"])
 loop = asyncio.get_event_loop()
 loop.run_until_complete(pk.run())
